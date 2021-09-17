1beb91cf3892422783984032d34e3ffc58e5abf3
nhamilton1 <nickh3592@gmail.com>
2021-09-17 12:51:01 -0400

    figured out what was wrong with my bubblepage.test.js, had to pass data for both

--- src/components/BubblePage.test.js
+++ src/components/BubblePage.test.js
@@ -5,23 +5,28 @@ import { render, screen } from "@testing-library/react";
 import BubblePage from './BubblePage';
 
 import fetchColorServices from '../services/fetchColorService'
+
 jest.mock('../services/fetchColorService')
 
+const testData = {
+    data:[
+        {color: 'blue', code: { hex: '#0000FF'}, id: 1},
+        {color: 'orange', code: { hex: '#FFA500'}, id: 2},
+        {color: 'purple', code: { hex: '#A020F0'}, id: 3},
+        {color: 'aqua', code: { hex: '#00FFFF'}, id: 4},
+        {color: 'pink', code: { hex: '#FFC0CB'}, id: 5},
+        {color: 'beige', code: { hex: '#F5F5DC'}, id: 6},
+    ]
+}
+
 test("Renders without errors", ()=> {
+    fetchColorServices.mockResolvedValueOnce(testData)
+    
     render(<BubblePage/>)
 });
 
 test("Renders appropriate number of colors passed in through mock", async ()=> {
-    fetchColorServices.mockResolvedValueOnce({
-        data:[
-            {color: 'blue', code: { hex: '#0000FF'}, id: 1},
-            {color: 'orange', code: { hex: '#FFA500'}, id: 2},
-            {color: 'purple', code: { hex: '#A020F0'}, id: 3},
-            {color: 'aqua', code: { hex: '#00FFFF'}, id: 4},
-            {color: 'pink', code: { hex: '#FFC0CB'}, id: 5},
-            {color: 'beige', code: { hex: '#F5F5DC'}, id: 6},
-        ]
-    })
+    fetchColorServices.mockResolvedValueOnce(testData)
 
     //ARRANGE: 
     render(<BubblePage/>)
