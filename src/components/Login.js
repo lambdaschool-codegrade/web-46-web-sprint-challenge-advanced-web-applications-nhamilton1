04cce99efca70626e27c5761ba3a0d28ecfa9a93
nhamilton1 <nickh3592@gmail.com>
2021-09-17 07:44:09 -0400

    building out login page and added routes

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,17 +1,54 @@
-import React from "react";
+import axios from "axios";
+import React, { useState } from "react";
+
+const initialFormValues = {
+  username: '',
+  password: ''
+}
 
 const Login = () => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
+  const [formValues, setFormValues] = useState(initialFormValues)
 
   const error = "";
   //replace with error state
+
+  const handleChange = (e) => {
+    setFormValues({...formValues,[e.target.name]: e.target.value})
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    axios.post('http://localhost:5000/api/login', formValues)
+      .then(res => {
+        console.log(res)
+        // localStorage.setItem('token', res.data.payload)
+      })
+  }
+
 
   return (
     <div>
       <h1>Welcome to the Bubble App!</h1>
       <div data-testid="loginForm" className="login-form">
-        <h2>Build login form here</h2>
+        <form onSubmit={handleSubmit}>
+          <input
+            type='text'
+            name='username'
+            id='username'
+            value={formValues.username}
+            onChange={handleChange}
+          />
+          <input
+            type='password'
+            name='password'
+            id='password'
+            value={formValues.password}
+            onChange={handleChange}
+          />
+          <button id='submit'>Log in</button>
+        </form>
       </div>
 
       <p id="error" className="error">{error}</p>
