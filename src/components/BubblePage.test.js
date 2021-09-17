eabb706b3b3a0df0033575906eac748a130f1147
nhamilton1 <nickh3592@gmail.com>
2021-09-17 12:34:26 -0400

    completed

--- src/components/BubblePage.test.js
+++ src/components/BubblePage.test.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import MutationObserver from 'mutationobserver-shim';
 
-import { render, screen} from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import BubblePage from './BubblePage';
 
 import fetchColorServices from '../services/fetchColorService'
 jest.mock('../services/fetchColorService')
 
-// test("Renders without errors", ()=> {
-//     render(<BubblePage />)
-// });
+test("Renders without errors", ()=> {
+    render(<BubblePage/>)
+});
 
 test("Renders appropriate number of colors passed in through mock", async ()=> {
     //Keep in mind that our service is called on mount for this component.
